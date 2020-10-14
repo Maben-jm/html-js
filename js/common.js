c1fd7a432422744bcbbcb1796bfe24608712e95f
Maben_jm@aliyun.com <Maben_jm@aliyun.com>
2020-10-14 13:57:38 +0800

    js学习事件学习

--- js/common.js
+++ js/common.js
@@ -54,12 +54,49 @@ function getPosition(ev) {
  * @param eventName 事件名称(不带on)
  * @param func 绑定函数
  */
-function boundEvent(oParent,eventName,func){
-    if (oParent.attachEvent){
+function boundEvent(oParent, eventName, func) {
+    if (oParent.attachEvent) {
         // IE
-        oParent.attachEvent("on"+eventName,func);
-    } else{
+        oParent.attachEvent("on" + eventName, func);
+    } else {
         // FIREFOX  || CHROME
-        oParent.addEventListener(eventName,func,false);
+        oParent.addEventListener(eventName, func, false);
     }
 }
+
+/**
+ * 发送ajax请求
+ * @param funType 方法类型 post || get
+ * @param funUrl 方法请求路径
+ * @param isAsynchronous 是否异步
+ * @param funSuccessCallBack 请求成功函数
+ * @param funErrorCallBack 请求失败函数
+ */
+function sendAjax(funType, funUrl, isAsynchronous, funSuccessCallBack, funErrorCallBack) {
+//    1.创建ajax对象
+    if (window.XMLHttpRequest) {
+        var oAjax = new XMLHttpRequest();
+    } else {
+        var oAjax = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+//    2.连接服务器
+    oAjax.open(funType, funUrl, isAsynchronous);
+//    3.发送请求
+    oAjax.send();
+//    4.接收返回
+    oAjax.onreadystatechange = function () {
+        //请求读取完成
+        if (oAjax.readyState === 4) {
+            if (oAjax.status === 200) {
+                if (funSuccessCallBack) {
+                    funSuccessCallBack(eval(oAjax.responseText));
+                }
+            } else {
+                if (funErrorCallBack) {
+                    funErrorCallBack(oAjax.status);
+                }
+            }
+        }
+    }
+}
+
